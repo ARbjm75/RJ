830996479cc0218a9c4357758350ce8003e03ef5
AR <ar.bjm75@gmail.com>
2025-09-21 14:44:32 +0800

    Add files via upload

--- lapse.mjs
+++ lapse.mjs
@@ -1710,8 +1710,10 @@ export async function kexploit() {
     }
     
      if (localStorage.ExploitLoaded === "yes" && sessionStorage.ExploitLoaded!="yes") {
-           runBinLoader();
-            return new Promise(() => {});
+		   setTimeout(PayloadLoader("aio_patches.bin"),500);
+		   setTimeout(PayloadLoader("goldhen_2.4b18.6.bin"),500);
+		   msgs.innerHTML = "GoldHEN v2.4b18.6 Loaded ...";
+		   return new Promise(() => {});
       }
  
     // fun fact:
@@ -1866,7 +1868,7 @@ kexploit().then(() => {
 setTimeout(PayloadLoader("aio_patches.bin"),500);
 log("AIO Fixes Applied.!");
 //Load GoldHEN :)
-setTimeout(PayloadLoader("goldhen_2.4b18.5.bin"),500);
-log("AR Playstation");
+setTimeout(PayloadLoader("goldhen_2.4b18.6.bin"),500);
+msgs.innerHTML = "HEN BERHASIL Ty AR Playstation";
 
 })
